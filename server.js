230b88a17bba195f3a7dbe5f51adfe04c4437897
SaymonAlex28 <145854670+SaymonAlex28@users.noreply.github.com>
2026-01-15 21:30:09 +0200

    commit7

--- server.js
+++ server.js
@@ -1,26 +1,27 @@
 import http from "node:http";
-import fetch from "node-fetch";
+import https from "node:https";
 
 const PORT = process.env.PORT || 10000;
 
-http.createServer(async (req, res) => {
+http.createServer((req, res) => {
   if (req.url === "/ecb") {
-    try {
-      const response = await fetch(
-        "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"
-      );
-      const data = await response.text();
-
-      // Добавляем CORS заголовок
-      res.writeHead(200, {
-        "Content-Type": "application/xml",
-        "Access-Control-Allow-Origin": "*" // <- это разрешает все домены
-      });
-      res.end(data);
-    } catch (err) {
+    https.get(
+      "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml",
+      (response) => {
+        let data = "";
+        response.on("data", (chunk) => (data += chunk));
+        response.on("end", () => {
+          res.writeHead(200, {
+            "Content-Type": "application/xml",
+            "Access-Control-Allow-Origin": "*", // <- разрешаем CORS
+          });
+          res.end(data);
+        });
+      }
+    ).on("error", (err) => {
       res.writeHead(500, { "Content-Type": "text/plain" });
       res.end("Ошибка: " + err.message);
-    }
+    });
   } else {
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.end("Not found");
